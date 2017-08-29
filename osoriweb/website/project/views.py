d5c3957d943eaf9a952aa0d44351e9cad18e7834
Camelia <camelia0858@gmail.com>
2017-08-29 22:15:40 +0900

    projects tab without css

--- osoriweb/website/project/views.py
+++ osoriweb/website/project/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
+from .models import Project
 
 def project(request):
     return redirect('proj_doing')
 
 def proj_doing(request):
-    return render(request, 'project/proj_doing.html', {})
+    projects = Project.objects.filter(end_period__isnull=True).order_by('start_period')
+    return render(request, 'project/proj_doing.html', {'projects': projects})
 
 def proj_done(request):
-    return render(request, 'project/proj_done.html', {})
+    projects = Project.objects.filter(end_period__isnull=False).order_by('start_period')
+    return render(request, 'project/proj_done.html', {'projects': projects})
